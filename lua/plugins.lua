761eed7e79200c67882ad4ac3f0bbe10f747e116
Bomou He <paakmau@outlook.com>
2021-11-15 22:15:14 +0800

    Encapsulate plugins.lua

---
+++ lua/plugins.lua
@@ -0,0 +1,41 @@
+return require('packer').startup(function(use)
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require'nvim-tree'.setup {}
+        end
+    }
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function()
+            require('lualine').setup {}
+        end
+    }
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = 'nvim-lua/plenary.nvim',
+        config = function()
+            require('gitsigns').setup {}
+        end
+    }
+
+    use {
+        'lukas-reineke/indent-blankline.nvim',
+        config = function()
+            require("indent_blankline").setup {}
+        end
+    }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
+end)
