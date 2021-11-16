a2a39050d7ecb3a5d5c20399feebdb802015ccad
Bomou He <paakmau@outlook.com>
2021-11-16 10:54:14 +0800

    Config indent-blankline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -42,7 +42,12 @@ return require('packer').startup(function(use)
     use {
         'lukas-reineke/indent-blankline.nvim',
         config = function()
-            require("indent_blankline").setup {}
+            require("indent_blankline").setup {
+                char = "▏",
+                show_first_indent_level = false,
+                filetype_exclude = {'help', 'git', 'markdown', 'text', 'terminal', 'lspinfo', 'packer'},
+                buftype_exclude = {'terminal', 'nofile'}
+            }
         end
     }
 
