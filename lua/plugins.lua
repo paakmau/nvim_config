42b0d05bcfd8ab9fdb35af8ad2eb128ecf84bc78
Bomou He <paakmau@outlook.com>
2021-11-19 15:50:26 +0800

    Adjust order of plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -100,6 +100,29 @@ return require('packer').startup(function(use)
     }
 
     use {
+        'williamboman/nvim-lsp-installer',
+        config = function()
+            local lsp_installer_servers = require('nvim-lsp-installer.servers')
+
+            local servers = {{'clangd', {}}, {'cmake', {}}, {'jdtls', {}}, {'sumneko_lua', {}}, {'rust_analyzer', {}}}
+
+            for _, lsp in ipairs(servers) do
+                local server_available, requested_server = lsp_installer_servers.get_server(lsp[1])
+                if server_available then
+                    requested_server:on_ready(function()
+                        requested_server:setup(lsp[2])
+                    end)
+                    if not requested_server:is_installed() then
+                        -- Queue the server to be installed
+                        requested_server:install()
+                    end
+                end
+            end
+
+        end
+    }
+
+    use {
         'neovim/nvim-lspconfig',
         config = function()
             local nvim_lsp = require('lspconfig')
@@ -158,29 +181,6 @@ return require('packer').startup(function(use)
                     name = 'vsnip'
                 }})
             })
-        end
-    }
-
-    use {
-        'williamboman/nvim-lsp-installer',
-        config = function()
-            local lsp_installer_servers = require('nvim-lsp-installer.servers')
-
-            local servers = {{'clangd', {}}, {'cmake', {}}, {'jdtls', {}}, {'sumneko_lua', {}}, {'rust_analyzer', {}}}
-
-            for _, lsp in ipairs(servers) do
-                local server_available, requested_server = lsp_installer_servers.get_server(lsp[1])
-                if server_available then
-                    requested_server:on_ready(function()
-                        requested_server:setup(lsp[2])
-                    end)
-                    if not requested_server:is_installed() then
-                        -- Queue the server to be installed
-                        requested_server:install()
-                    end
-                end
-            end
-
         end
     }
 
