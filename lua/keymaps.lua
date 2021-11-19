7c3232eb73df002af515bb8325a07f8c32b04f00
Bomou He <paakmau@outlook.com>
2021-11-19 16:07:43 +0800

    Config keymaps to navigate windows

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,19 @@ local opts = {
 }
 
 map('n', '<Leader>e', ':NvimTreeToggle<CR>', opts)
+
+map('t', '<esc>', [[<C-\><C-n>]], opts)
+map('t', 'jk', [[<C-\><C-n>]], opts)
+
+map('i', '<C-h>', [[<esc><C-W>h]], opts)
+map('i', '<C-j>', [[<esc><C-W>j]], opts)
+map('i', '<C-k>', [[<esc><C-W>k]], opts)
+map('i', '<C-l>', [[<esc><C-W>l]], opts)
+map('n', '<C-h>', [[<C-W>h]], opts)
+map('n', '<C-j>', [[<C-W>j]], opts)
+map('n', '<C-k>', [[<C-W>k]], opts)
+map('n', '<C-l>', [[<C-W>l]], opts)
+map('t', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
+map('t', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
+map('t', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+map('t', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
