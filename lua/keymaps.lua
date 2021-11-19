a9ce068fbc3f3f9d6eaa2c543098bdc1cf8bee21
Bomou He <paakmau@outlook.com>
2021-11-19 18:32:10 +0800

    Config keymaps to switch tabs

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,6 +5,16 @@ local opts = {
 
 map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
 
+map('n', '<leader>1', ':BufferLineGoToBuffer 1<CR>', opts)
+map('n', '<leader>2', ':BufferLineGoToBuffer 2<CR>', opts)
+map('n', '<leader>3', ':BufferLineGoToBuffer 3<CR>', opts)
+map('n', '<leader>4', ':BufferLineGoToBuffer 4<CR>', opts)
+map('n', '<leader>5', ':BufferLineGoToBuffer 5<CR>', opts)
+map('n', '<leader>6', ':BufferLineGoToBuffer 6<CR>', opts)
+map('n', '<leader>7', ':BufferLineGoToBuffer 7<CR>', opts)
+map('n', '<leader>8', ':BufferLineGoToBuffer 8<CR>', opts)
+map('n', '<leader>9', ':BufferLineGoToBuffer 9<CR>', opts)
+
 map('t', '<esc>', [[<C-\><C-n>]], opts)
 map('t', 'jk', [[<C-\><C-n>]], opts)
 
