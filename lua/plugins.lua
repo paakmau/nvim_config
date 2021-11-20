8164e84f6e0b808fbd1fdad0b75cf1c95bd45944
Bomou He <paakmau@outlook.com>
2021-11-20 15:13:48 +0800

    Config clang-format for formatter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -231,6 +231,16 @@ return require("packer").startup(function(use)
         config = function()
             require("formatter").setup({
                 filetype = {
+                    cpp = {
+                        function()
+                            return {
+                                exe = "clang-format",
+                                args = { "--assume-filename", vim.api.nvim_buf_get_name(0) },
+                                stdin = true,
+                                cwd = vim.fn.expand("%:p:h"),
+                            }
+                        end,
+                    },
                     lua = {
                         function()
                             return {
