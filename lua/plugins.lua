80ddd5357359e0d302f291e74248b676c7b57b99
Bomou He <paakmau@outlook.com>
2021-11-20 23:17:45 +0800

    Config gitsigns

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -143,8 +143,6 @@ return require("packer").startup(function(use)
         requires = "nvim-lua/plenary.nvim",
         config = function()
             require("gitsigns").setup({
-                signcolumn = false,
-                numhl = true,
                 current_line_blame = true,
                 current_line_blame_opts = {
                     delay = 0,
