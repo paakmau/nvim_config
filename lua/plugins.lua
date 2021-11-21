b0ac2209c4b0bbd7d3503dfb7bcb8b52d800178b
Bomou He <paakmau@outlook.com>
2021-11-22 01:04:05 +0800

    Make packer use a floating window

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -398,4 +398,9 @@ return require("packer").startup({
             require("packer").sync()
         end
     end,
+    config = {
+        display = {
+            open_fn = require("packer.util").float,
+        },
+    },
 })
