ae154bf8417d6b0d3ae9d383523e547050f201c1
Bomou He <paakmau@outlook.com>
2021-11-22 01:15:59 +0800

    Integrate telescope with fzf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -132,6 +132,8 @@ return require("packer").startup({
             end,
         })
 
+        use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+
         use({
             "nvim-telescope/telescope.nvim",
             requires = "nvim-lua/plenary.nvim",
@@ -148,7 +150,20 @@ return require("packer").startup({
                 map("n", "<leader>fp", "<cmd>Telescope projects<CR>", opts)
                 map("n", "<leader>fc", "<cmd>Telescope commands<CR>", opts)
 
+                require("telescope").setup({
+                    extensions = {
+                        fzf = {
+                            fuzzy = true, -- false will only do exact matching
+                            override_generic_sorter = true, -- override the generic sorter
+                            override_file_sorter = true, -- override the file sorter
+                            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+                            -- the default case_mode is "smart_case"
+                        },
+                    },
+                })
+
                 require("telescope").load_extension("projects")
+                require("telescope").load_extension("fzf")
             end,
         })
 
