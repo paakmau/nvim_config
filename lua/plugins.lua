23bcc1a4d3fbb64ab940cf7773b6327d356e7d40
Bomou He <paakmau@outlook.com>
2021-11-22 18:38:05 +0800

    Modify keymaps to navigate buffers

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,19 +50,19 @@ return require("packer").startup({
                 local opts = {
                     noremap = true,
                 }
-                map("n", "<leader>bh", ":BufferLineCyclePrev<CR>", opts)
-                map("n", "<leader>bl", ":BufferLineCycleNext<CR>", opts)
-                map("n", "<leader>bH", ":BufferLineMovePrev<CR>", opts)
-                map("n", "<leader>bL", ":BufferLineMoveNext<CR>", opts)
-                map("n", "<leader>b1", ":BufferLineGoToBuffer 1<CR>", opts)
-                map("n", "<leader>b2", ":BufferLineGoToBuffer 2<CR>", opts)
-                map("n", "<leader>b3", ":BufferLineGoToBuffer 3<CR>", opts)
-                map("n", "<leader>b4", ":BufferLineGoToBuffer 4<CR>", opts)
-                map("n", "<leader>b5", ":BufferLineGoToBuffer 5<CR>", opts)
-                map("n", "<leader>b6", ":BufferLineGoToBuffer 6<CR>", opts)
-                map("n", "<leader>b7", ":BufferLineGoToBuffer 7<CR>", opts)
-                map("n", "<leader>b8", ":BufferLineGoToBuffer 8<CR>", opts)
-                map("n", "<leader>b9", ":BufferLineGoToBuffer 9<CR>", opts)
+                map("n", "<leader>p", ":BufferLineCyclePrev<CR>", opts)
+                map("n", "<leader>n", ":BufferLineCycleNext<CR>", opts)
+                map("n", "<leader>P", ":BufferLineMovePrev<CR>", opts)
+                map("n", "<leader>N", ":BufferLineMoveNext<CR>", opts)
+                map("n", "<leader>1", ":BufferLineGoToBuffer 1<CR>", opts)
+                map("n", "<leader>2", ":BufferLineGoToBuffer 2<CR>", opts)
+                map("n", "<leader>3", ":BufferLineGoToBuffer 3<CR>", opts)
+                map("n", "<leader>4", ":BufferLineGoToBuffer 4<CR>", opts)
+                map("n", "<leader>5", ":BufferLineGoToBuffer 5<CR>", opts)
+                map("n", "<leader>6", ":BufferLineGoToBuffer 6<CR>", opts)
+                map("n", "<leader>7", ":BufferLineGoToBuffer 7<CR>", opts)
+                map("n", "<leader>8", ":BufferLineGoToBuffer 8<CR>", opts)
+                map("n", "<leader>9", ":BufferLineGoToBuffer 9<CR>", opts)
 
                 require("bufferline").setup({
                     options = {
